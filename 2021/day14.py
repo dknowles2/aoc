4b0ddf700822229486139ab7a277decf1ab31f91
David Knowles <dknowles2@gmail.com>
2021-12-23 19:10:50 -0500

    Complete day 14 that's less dumb

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
-from collections import Counter
+from collections import defaultdict as ddict
+import os
 
-with open("input/" + __file__.replace(".py", ".txt")) as f:
+def get_input():
+    return os.path.join(
+        os.path.dirname(__file__),
+        "input",
+        os.path.basename(__file__).replace(".py", ".txt"))
+
+
+with open(get_input()) as f:
     in1 = f.read()
+
 
 ex1 = """\
 NNCB
@@ -30,120 +39,52 @@ CN -> C
 def parse(xin):
     arrow = ' -> '
     tmpl = None
-    pairs = {}
+    rules = {}
     for l in xin.strip().split('\n'):
         if not l:
             continue
         if arrow in l:
             pair, ins = l.split(arrow)
-            pairs[pair] = ins
+            rules[pair] = ins
         else:
             tmpl = l
-    return tmpl, pairs
-
-
-def one(xin, steps=10, debug=False):
-    poly, pairs = parse(xin)
-    if debug:
-        print(f'Template:      {poly}')
-
-    for s in range(steps):
-        new_poly = []
-        i = 1
-        while i < len(poly):
-            p = poly[i-1:i+1]
-            new_poly.append(p[0] + pairs.get(p, ''))
-            i+=1
-        new_poly.append(poly[-1])
-        poly = ''.join(new_poly)
-        if debug:
-            print(f'after step {s+1:2}: {poly[:40]}')
-    c = Counter(poly)
-    mc = c.most_common()
-    print(mc[0][1] - mc[-1][1])
-
-
-def expand(pairs, pair, remain, depth=5):
-    print(pair, remain, depth)
-    if depth == 0:
-        c = Counter()
-        for i in range(len(remain)-1):
-            return expand(pairs, remain[i:i+2], remain[i+1:], 0)
-            print(remain[i:i+2])
-    else:
-        return expand(pairs, pair[0]+pairs[pair], pair[1]+remain, depth-1)
-
-
-def two(xin, steps=10, debug=False):
-    poly, pairs = parse(xin)
-
-    c = Counter()
-    for i in range(len(poly)-1):
-        pair = poly[i:i+2]
-        for s in range(steps):
-            npair = pair[0] + pairs[pair]
-            print(i, pair, s, npair)
-            if pair == npair:
-                c += Counter(pair)
-                pair = npair
-
-    print(c)
-
-    if True:
-        return
-    if debug:
-        print(f'Template:      {poly}')
-
-    c = Counter()
-    for s in range(steps):
-        new_poly = []
-        i = 1
-        while i < len(poly):
-            p = poly[i-1:i+1]
-            print(f'..[{i}] {p} -> {p[0]}{pairs[p]}')
-            new_poly.append(p[0] + pairs.get(p, ''))
-            i+=1
-        new_poly.append(poly[-1])
-        poly = ''.join(new_poly)
-        print(f'after step {s+1:2}: {poly}')
-    c += Counter(poly)
-    mc = c.most_common()
-    print(mc[0][1] - mc[-1][1])
+    return tmpl, rules
 
 
 def solve(xin, steps=10, debug=False):
-    poly, pairs = parse(xin)
+    tmpl, rules = parse(xin)
     if debug:
-        print(f'Template:      {poly}')
+        print(f'Template:      {tmpl}')
 
-    poly = list(poly)
-    c = Counter()
+    def grow(pairs, rules):
+        new_pairs = ddict(int)
+        for p, c in pairs.items():
+            ins = rules[p]
+            new_pairs[p[0]+ins] += c
+            new_pairs[ins+p[1]] += c
+        return new_pairs
+
+    pairs = ddict(int)
+    for i in range(len(tmpl)-1):
+        pairs[tmpl[i]+tmpl[i+1]] += 1
+
     for s in range(steps):
-        i = 1
-        while i < len(poly):
-            p = ''.join(poly[i-1:i+1])
-            if i == 1 and pairs[p] == p[1]:
-                print(f'i={i}; p = {p}; pairs[p] = {pairs[p]}')
-                c += Counter(p)
-                poly = poly[i:]
-                i = 1
-                continue
-            else:
-                poly.insert(i, pairs[p])
-                i+=1
-            i+=1
-
+        pairs = grow(pairs, rules)
         if debug:
-            print(f'After step {s+1:2}: {"".join(poly)}')
-    c += Counter(poly)
-    mc = c.most_common()
-    print(mc)
-    print(mc[0][1] - mc[-1][1])
+            print(f"after step {s+1:2}: {1+sum(pairs.values())}")
+
+    ltrs = ddict(int, {tmpl[0]: 1, tmpl[-1]: 1})
+    for p, c in pairs.items():
+        ltrs[p[0]] += c
+        ltrs[p[1]] += c
+
+    most = 0
+    least = float('inf')
+    for v in ltrs.values():
+        most = max(most, v)
+        least = min(least, v)
+    print(int((most - least) / 2))
 
 
-one(ex1, steps=10, debug=True)
-# solve(ex1, steps=10, debug=True)
-# solve(in1, steps=10, debug=True)
-print("====two")
-two(ex1, steps=4, debug=True)
-# solve(in1, steps=20, debug=True)
+smart(in1, steps=10)
+smart(in1, steps=40)
