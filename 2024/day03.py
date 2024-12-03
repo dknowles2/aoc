11482b924a367f8b7e1a19e086d903ddd862079b
David Knowles <dknowles2@gmail.com>
2024-12-03 08:42:40 -0500

    Day 3

---
+++ 2024/day03.py
@@ -0,0 +1,65 @@
+import re
+
+from aoc import solution
+from pytest import fixture
+
+
+@fixture
+def example1():
+    return """\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+""".splitlines()
+
+
+@fixture
+def example2():
+    return """\
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+""".splitlines()
+
+
+_MUL_RE = re.compile(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)$")
+_DO_RE = re.compile(r"do\(\)$")
+_DONT_RE = re.compile(r"don't\(\)$")
+_RESET_RE = re.compile(r".*(mul|do|don't)$")
+
+
+@solution(161, 178794710)
+def one(puzzle_input):
+    ret = 0
+    word = ""
+    for c in "".join(puzzle_input):
+        word += c
+        if len(word) >= 3 and word[-3:] == "mul":
+            word = "mul"
+            continue
+        if len(word) < 8:
+            continue
+        if match := _MUL_RE.match(word):
+            a, b = match.groups()
+            ret += int(a) * int(b)
+            word = ""
+    return ret
+
+
+@solution(48, 76729637)
+def two(puzzle_input):
+    enabled = True
+    ret = 0
+    word = ""
+    for c in "".join(puzzle_input):
+        word += c
+        if m := _RESET_RE.match(word):
+            word = word[-len(m.groups()[0]) :]
+            continue
+        if _DONT_RE.match(word):
+            enabled = False
+            word = ""
+        elif _DO_RE.match(word):
+            enabled = True
+            word = ""
+        elif enabled and (m := _MUL_RE.match(word)):
+            a, b = m.groups()
+            ret += int(a) * int(b)
+            word = ""
+    return ret
--- conftest.py
+++ conftest.py
@@ -1,13 +1,28 @@
 from pathlib import Path
 from typing import Any
 
-from pytest import FixtureRequest, fixture
+from pytest import FixtureLookupError, FixtureRequest, fixture
 
 
 @fixture
-def puzzle_input(request: FixtureRequest, example: Any):
+def example_wrapper(request: FixtureRequest):
+    try:
+        return request.getfixturevalue("example")
+    except FixtureLookupError:
+        pass
+    for test_name in request.keywords:
+        break
+    if test_name.startswith("one["):
+        return request.getfixturevalue("example1")
+    elif test_name.startswith("two["):
+        return request.getfixturevalue("example2")
+    raise FixtureLookupError
+
+
+@fixture
+def puzzle_input(request: FixtureRequest, example_wrapper: Any):
     if "example" in request.keywords:
-        return example
+        return example_wrapper
     elif "puzzle" in request.keywords:
         me = Path(request.path)
         input_file = Path(me.parent / "input" / f"{me.stem.replace('test_', '')}.txt")
